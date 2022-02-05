d1e8b00b4587b4b72ee9ed1cd2f7e1b971419172
yadav-rohit <49914319+yadav-rohit@users.noreply.github.com>
2022-02-05 22:50:23 +0530

    Cors fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const connectToMongo = require("./db");
+var cors = require('cors');
 
 connectToMongo();
 
@@ -6,11 +7,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 //Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`zapbook backend listening on port ${port}`);
 });
